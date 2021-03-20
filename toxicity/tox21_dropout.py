9e7062948bee1d1c54457b08144d386f86f12c9a
LanceKnight <lanceknight26@gmail.com>
2021-03-20 15:40:39 -0500

    make tox21_dropout.py working for dataset with extra data

--- toxicity/tox21_dropout.py
+++ toxicity/tox21_dropout.py
@@ -27,13 +27,14 @@ hidden_activation = Softmax()#Tanh()
 conv_depth = 5
 dropout_rate = 0.2
 scaled_unsupervised_weight = 100
-target_col = [x for x in range(0,6)]
+target_col =[0] #[x for x in range(6,12)]
 
 print(f"target_col:{target_col}")
 Tox21 = MoleculeNet(root = "../data/raw/Tox21", name = "Tox21")
 #print("data info:")
 #print("============")
 #print(f"num of data:{len(Tox21)}")
+
 
 num_data = len(Tox21)
 train_num = 1000#int(num_data * 0.8)
@@ -43,7 +44,7 @@ print(f"train_num = {train_num}, val_num = {val_num}, test_num = {test_num}")
 
 train_loader = DataLoader(Tox21[:train_num], batch_size = batch_size, shuffle = True)
 validate_loader = DataLoader(Tox21[train_num:train_num+val_num], batch_size = batch_size, shuffle = False)
-test_loader = DataLoader(Tox21[-test_num:], batch_size = test_num, shuffle = False)
+test_loader = DataLoader(Tox21[7831-test_num:7831], batch_size = test_num, shuffle = False)
 
 class AtomBondConv(MessagePassing):
 	def __init__(self, x_dim, edge_attr_dim):
@@ -111,13 +112,6 @@ device = torch.device('cuda' if is_cuda else 'cpu')
 model = MyNet(num_node_features, num_edge_features, conv_depth).to(device)
 #criterion = torch.nn.BCELoss()
 
-def rampup(epoch):
-    if epoch < config.rampup_length:
-        p = max(0.0, float(epoch)) / float(config.rampup_length)
-        p = 1.0 - p
-        return math.exp(-p*p*5.0)
-    else:
-        return 1.0
 
 def BCELoss_no_NaN(out, target):
 	#print(f"out.shape:{out.shape}             target.shape:{target.shape}")
@@ -126,7 +120,7 @@ def BCELoss_no_NaN(out, target):
 	#print(f"target_no_NaN:{target_no_NaN}")
 	return torch.nn.BCELoss()(out, target_no_NaN)
 
-def train(data_loader, debug_mode, target_col, rampup_val):
+def train(data_loader, debug_mode, target_col):
 	model.train()
 	for data in data_loader:
 		#print(f"smi:{data.smiles}")
@@ -138,33 +132,30 @@ def train(data_loader, debug_mode, target_col, rampup_val):
 	
 		#print(f"data.x:{data.x.shape}")
 		#print(f"data.edge_attr:{data.edge_attr.shape}")
-		out1 = model(data.x.float(), data.edge_index, data.edge_attr, data.smiles, data.batch)# use our own x and edge_attr instead of data.x and data.edge_attr
-		out1 = out.view(len(data.y[:,target_col]))
-
-		out2 = model(data.x.float(), data.edge_index, data.edge_attr, data.smiles, data.batch)# use our own x and edge_attr instead of data.x and data.edge_attr
-		out2 = out.view(len(data.y[:,target_col]))
+		out = model(data.x.float(), data.edge_index, data.edge_attr, data.smiles, data.batch)# use our own x and edge_attr instead of data.x and data.edge_attr
+		out = out.view(len(data.y[:,target_col]))
+		#print(out)
+		#out2 = model(data.x.float(), data.edge_index, data.edge_attr, data.smiles, data.batch)# use our own x and edge_attr instead of data.x and data.edge_attr
+		#out2 = out.view(len(data.y[:,target_col]))
 		#print(f"out.shape:{out.shape},           y.shape{data.y[:, target_col].shape}")
 		#print(f"out:{out}\n y:\n{data.y[:,target_col]}")
 		loss = BCELoss_no_NaN(out, data.y[:,target_col])
-		unsupervised_loss = torch.nn.MSELoss(out1, out2)
-		unsupervised_weight = rampup_val * scaled_unsupervised_weight
-		total_loss = loss + unsupervised_weight * unsupervised_loss
+		#unsupervised_loss = torch.nn.MSELoss(out1, out2)
+		#unsupervised_weight = rampup_val * scaled_unsupervised_weight
+		total_loss = loss #+ unsupervised_weight * unsupervised_loss
 		#print(f"loss:{loss}")
 		total_loss.backward()
 		optimizer.step()
 		optimizer.zero_grad()
+
 		if(debug_mode):
-			out_list = out.cpu().detach().numpy()
-			y_list = data.y.cpu().detach().numpy()
-			#print(f"{len(out_list)}, {len(y_list)}")
-			for i in range(len(out_list)): 
-				print(f"{out_list[i][0]}, {y_list[i][0]}") # for making correlation plot
+			pass
 
 def test(data_loader, debug_mode, target_col):
 	model.eval()
-
 	auc_lst = []
 	for data in data_loader:
+
 		x, edge_attr = batch2attributes(data.smiles, molecular_attributes= True)
 		data.x = x
 		data.edge_attr = edge_attr
@@ -175,14 +166,11 @@ def test(data_loader, debug_mode, target_col):
 		#==========convert to numpy array
 		out = out.view(len(out))	
 		out = out.cpu().detach().numpy()
-		#print(f"out:{out}")
 		y = data.y[:,target_col]
 		y = y.view(len(y)).cpu().detach().numpy()
-		#print(f"y:{y}")
 		#==========remove NaN
 		out = out[~np.isnan(y)]
 		y = y[~np.isnan(y)]
-
 		#print(f"data.y.shape:{y}   out.shape:{out})")
 		sc = roc_auc_score(y, out)
 		auc_lst.append(sc)
@@ -230,8 +218,8 @@ for col in target_col:
 	test_sc = 0
 	for epoch in tqdm(range(num_epoches)):
 		optimizer = torch.optim.Adam(model.parameters(), lr = 0.0007 * math.exp(-epoch/30 ))#, weight_decay = 5e-4)
-		rampup_val = rampup(epoch)
-		train(train_loader, False, col, rampup_val)#epoch==(num_epoches-1))
+		#rampup_val = rampup(epoch)
+		train(train_loader, False, col)#epoch==(num_epoches-1))
 		#train_sc = test(train_loader, False)#  epoch==(num_epoches-1))
 		test_sc = test(test_loader, False, col)# epoch==(num_epoches-1))
 		#print(f"Epoch:{epoch:03d}, Train AUC:{train_sc: .4f}, Test AUC:{test_sc: .4f}")

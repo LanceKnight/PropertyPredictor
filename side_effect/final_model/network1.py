a0daf6e72eb0397537ea81ab065cc81102b719c5
LanceKnight <lanceknight26@gmail.com>
2021-04-17 16:55:01 -0500

    rename to network1.py

--- side_effect/final_model/network1.py
+++ side_effect/final_model/network1.py
@@ -4,15 +4,23 @@ from torch_geometric.nn import MessagePassing, global_add_pool
 from torch_geometric.utils import add_self_loops
 
 
+
+
 class AtomBondConv(MessagePassing):
-	def __init__(self, x_dim, edge_attr_dim, dropout_rate):
+	def __init__(self, x_dim, edge_attr_dim):
 		super(AtomBondConv, self).__init__(aggr = 'add')
 		self.W_in = Linear(x_dim + edge_attr_dim, x_dim)
-		self.dropout = Dropout(dropout_rate)
-	def forward(self, x, edge_index, edge_attr, smiles, batch):		
+		#self.dropout = torch.nn.Dropout(dropout_rate)
+		self.unsup_dropout = torch.nn.Dropout(unsup_dropout_rate)
+		self.sup_dropout = torch.nn.Dropout(sup_dropout_rate)
+	def forward(self, x, edge_index, edge_attr, smiles, batch, is_supervised ):		
+		if is_supervised:
+			dropout = self.sup_dropout
+		else:
+			dropout = self.unsup_dropout
 		edge_index, _ = add_self_loops(edge_index, num_nodes = x.size(0))
 		x = self.propagate(edge_index, x = x, edge_attr = edge_attr)
-		x = self.dropout(self.W_in(x))
+		x = dropout(self.W_in(x))
 		return x
 
 	def message(self, x, x_j, edge_attr):
@@ -23,26 +31,30 @@ class AtomBondConv(MessagePassing):
 
 
 class MyNet(torch.nn.Module):
-	def __init__(self, num_node_features, num_edge_features, inner_atom_dim, depth, dropout_rate):
+	def __init__(self, num_node_features, num_edge_features, depth):
 		super(MyNet, self).__init__()
-		self.atom_bond_conv = AtomBondConv(num_node_features, num_edge_features, dropout_rate)
+		self.atom_bond_conv = AtomBondConv(num_node_features, num_edge_features)
 		self.W_out = Linear(num_node_features, inner_atom_dim)
 		self.lin1 = Linear(inner_atom_dim, 50)
 		self.lin2 = Linear(50, 1)
 		self.depth = depth
-		self.dropout = torch.nn.Dropout(dropout_rate)
-
-	def forward(self, x, edge_index, edge_attr, smiles, batch):
+		self.unsup_dropout = torch.nn.Dropout(unsup_dropout_rate)
+		self.sup_dropout = torch.nn.Dropout(sup_dropout_rate)
+	def forward(self, x, edge_index, edge_attr, smiles, batch, is_supervised):
+		if is_supervised:
+			dropout = self.sup_dropout
+		else:
+			dropout = self.unsup_dropout
 		molecule_fp_lst = []
 		for i in range(0, self.depth+1):
 			atom_fp = Softmax(dim=1)(self.W_out(x))	
 			molecule_fp = global_add_pool(atom_fp, batch)
 			molecule_fp_lst.append(molecule_fp)
-			x = self.atom_bond_conv(x, edge_index, edge_attr, smiles, batch)
+			x = self.atom_bond_conv(x, edge_index, edge_attr, smiles, batch, is_supervised)
 
 		overall_molecule_fp	= torch.stack(molecule_fp_lst, dim=0).sum(dim=0)	
-		hidden = self.dropout(self.lin1(overall_molecule_fp))
-		out = self.dropout(self.lin2(hidden))
+		hidden = dropout(self.lin1(overall_molecule_fp))
+		out = dropout(self.lin2(hidden))
 		return Sigmoid()(out)
 
 def Get_Net(num_node_features, num_edge_features, conv_depth, inner_atom_dim,  dropout_rate):

b5eaa4c5194a7b11642debe7092389a9e6161670
LanceKnight <lanceknight26@gmail.com>
2021-04-17 16:58:38 -0500

    add exception handler

--- side_effect/final_model/testing.py
+++ side_effect/final_model/testing.py
@@ -6,11 +6,12 @@ from molecule_processing import batch2attributes
 
 
 def roc_auc_score_one_class_compatible(y_true, y_predict):
-	if len(set(y_true)) == 1:
-		pass
-	else:
-		return roc_auc_score(y_true, y_predict)
-
+	sc = 9999
+	try:
+		sc = roc_auc_score(y_true, y_predict)
+	except:
+		print(f"error in running roc_auc_score(y_true, y_predict)   y_true:\n{y_true}, y_predict:\n{y_predict} sc:{sc}")
+	return sc 
 def pr_auc(y_true, y_predict):
 	precision, recall, _ = precision_recall_curve(y_true, y_predict)
 	auc_score = auc(recall, precision)	
@@ -26,7 +27,7 @@ def test(model, data_loader, debug_mode, target_col, device):
 		data.edge_attr = edge_attr
 		data.to(device)	
 
-		out = model(data.x.float(), data.edge_index, data.edge_attr, data.smiles, data.batch, True) # use our own x and edge_attr instead of data.x and data.edge_attr
+		out = model(data.x.float(), data.edge_index, data.edge_attr, data.smiles, data.batch, is_supervised = True) # use our own x and edge_attr instead of data.x and data.edge_attr
 
 		#==========convert to numpy array
 		out = out.view(len(out))	
@@ -40,38 +41,11 @@ def test(model, data_loader, debug_mode, target_col, device):
 		y = y[~np.isnan(y)]
 
 		#print(f"data.y.shape:{y}   out.shape:{out})")
-		sc = roc_auc_score_one_class_compatible(y, out)
-		#sc = pr_auc(y, out)
-		auc_lst.append(sc)
-		if(debug_mode):
-			#p = pred.cpu().detach().numpy()
-			#y = data.y.cpu().detach().numpy()
-			#for debugging
-#			print(f"pred:============")
-#			for i in range(len(p)):
-#				print(p[i][0])
-#			print(f"y:============")
-#			for i in range(len(y)):
-#				print(y[i][0])
-#			print(f"diff:============")
-#			for i in range(len(y)):
-
-
-#				print((p[i]-y[i])[0])
-#			print(f"pow:============")
-#			for i in range(len(y)):
-#				print((pow(p[i]-y[i],2))[0])
-#			print(f"sum=======")
-#			print(t)
-
-
-			# for plotting 
-			out_list = out.cpu().detach().numpy()
-			y_list = data.y.cpu().detach().numpy()
-			#print(f"{len(out_list)}, {len(y_list)}")
-			for i in range(len(out_list)): 
-				print(f"{out_list[i][0]}, {y_list[i][0]}") # for making correlation plot
-	if(debug_mode):
-		pass
-		#print(f"squared_error_sum: {squared_error_sum}, len:{num_samples}, MSE:{MSE}")	
-	return mean(auc_lst)
+		if ((len(y)!=0)  and (len(set(y))!=1)):
+			sc = roc_auc_score_one_class_compatible(y, out)
+			#sc = pr_auc(y, out)
+			auc_lst.append(sc)
+	try:
+		return mean(auc_lst)
+	except:
+		print(f"auc_lst:{auc_lst}")

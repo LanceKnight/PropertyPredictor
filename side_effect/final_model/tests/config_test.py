9f966fea0e99c6dd02e319460359ffd9cdc5fbc8
LanceKnight <lanceknight26@gmail.com>
2021-04-17 22:24:18 -0500

    update tests

--- side_effect/final_model/tests/config_test.py
+++ side_effect/final_model/tests/config_test.py
@@ -1,10 +1,11 @@
 from config_parser import set_config_file, get_config
 
 set_config_file('config.cfg')
-file1 = get_config('cfg','lst')
+#set_config_file('../inputs/comp/base-ssl/base-ssl-a.cfg')
+file1 = get_config('file','output_file')
 #file1 = get_config('file','file1')
 #file2 = get_config('file','file2')
-print(type(file1))
+print(file1)
 
 
 

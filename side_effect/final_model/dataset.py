ff25cf632c6260250c5460e8e5f138fd309b9ea3
LanceKnight <lanceknight26@gmail.com>
2021-04-26 18:26:33 -0500

    dataset use absolute path

--- side_effect/final_model/dataset.py
+++ side_effect/final_model/dataset.py
@@ -7,7 +7,9 @@ from torch_geometric.data import DataLoader
 #from printing import tee_print
 import printing
 
-SIDER = MoleculeNet(root = "../../data/raw/SIDER", name = "SIDER")# This is a combined dataset, the first 1427 samples are labeld from SIDER. Then 8597 sampes from ToxCast (19 of them were discarded due to the failure to convert to mol), 7831 samples were from Tox21. The total number of samples are 1427+8597+7831-19 = 17836
+SIDER = MoleculeNet(root = "home/liuy69/.clearml/venvs-builds/3.6/task_repository/PropertyPredictor.git/data/raw/SIDER", name="SIDER")
+
+#SIDER = MoleculeNet(root = "../../data/raw/SIDER", name = "SIDER")# This is a combined dataset, the first 1427 samples are labeld from SIDER. Then 8597 sampes from ToxCast (19 of them were discarded due to the failure to convert to mol), 7831 samples were from Tox21. The total number of samples are 1427+8597+7831-19 = 17836
 
 NUM_LABELED = 1427
 

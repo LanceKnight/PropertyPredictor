00ce09f571a711c84fa373a4237e2f5243bdadab
LanceKnight <lanceknight26@gmail.com>
2021-04-28 11:08:33 -0500

    simplified main.py

--- side_effect/final_model/main.py
+++ side_effect/final_model/main.py
@@ -20,47 +20,31 @@ from testing import test
 from network import build_model
 from param_grid_search import generate_param_sets, get_param_set, get_param_sets_length, record_result, save_file 
 
+
+# load the configuration file
 config_file = sys.argv[1]
 set_config_file(config_file)
 
+# some configurations need to be load before param set
 target_col = get_config('cfg','target_col')
-#
-#	inner_atom_dim = int(get_config('architecture','inner_atom_dim'))
-#	conv_depth = int(get_config('architecture','conv_depth'))
-#	sup_dropout_rate = float(get_config('architecture','sup_dropout_rate'))
-#
-#	batch_size = int(get_config('training','batch_size'))
-#	num_epochs = int(get_config('training','num_epochs'))
-#	patience = int(get_config('training','patience'))
-#
 use_SSL = bool(int(get_config('unsupervised', 'use_ssl')))
-#	unsup_dropout_rate = float(get_config('unsupervised','unsup_dropout_rate'))
-#	w = get_config('unsupervised','w')
-#	edge_dropout_rate = float(get_config('unsupervised','edge_dropout_rate'))
-#	rampup_length = int(get_config('unsupervised','rampup_length'))
-#if use_SSL == False:
-#	num_extra_data = 0
-#else:
-#	num_extra_data = int(get_config('unsupervised','num_extra_data'))
-#
-#	lr_init_lst = get_config('lr','lr_init')
-#	lr_base_lst = get_config('lr', 'lr_base')
-#	lr_exp_multiplier_lst = get_config('lr', 'lr_exp_multiplier')
+
+if use_SSL == False:
+	num_extra_data = 0
+else:
+	num_extra_data = int(get_config('unsupervised','num_extra_data'))
 
 output_file = get_config('file','output_file')
 final_auc_file = get_config('file','auc_file')
 auc_file_per_epoch = get_config('file', 'auc_file_per_epoch')
 
-
-
-model_config_dict = get_config_dict()
-#model_config_dict['num_extra_data']=num_extra_data
-#print(model_config_dict)
-generate_param_sets(model_config_dict)
-
+# set console output file for tee_print
 set_output_file(output_file)
 
-tee_print(f"target_col:{target_col} use_ssl:{use_SSL}")
+# generate param sets
+model_config_dict = get_config_dict()
+model_config_dict['num_extra_data']=num_extra_data
+generate_param_sets(model_config_dict)
 
 
 n = get_param_sets_length()
@@ -70,6 +54,7 @@ for i in range(n):
 	param_set = get_param_set(i)
 	print(param_set)
 	col = int(param_set['target_col'])
+	fold = int(param_set['fold'])
 
 	inner_atom_dim = int(param_set['inner_atom_dim'])
 	conv_depth = int(param_set['conv_depth'])
@@ -84,28 +69,22 @@ for i in range(n):
 	w =  int(param_set['w'])
 	edge_dropout_rate =  float(param_set['edge_dropout_rate'])
 	rampup_length =  int(param_set['rampup_length'])
-	if use_SSL == False:
-		num_extra_data = 0
-	else:
-		num_extra_data =  int(param_set['num_extra_data'])
+	num_extra_data =  int(param_set['num_extra_data'])
 
 	lr_init =  float(param_set['lr_init'])
 	lr_base =  float(param_set['lr_base'])
 	lr_exp_multiplier =  float(param_set[ 'lr_exp_multiplier'])
-	print(f"batch_size:{type(batch_size)}")
 
 	print('======')
 
 
-
-	train_loader, val_loader, test_loader = get_loaders_with_idx(num_extra_data, batch_size, 0)#get_loaders(num_extra_data, batch_size)
+	# load data
+	train_loader, val_loader, test_loader = get_loaders_with_idx(num_extra_data, batch_size, 0)
 			
 			
 	is_cuda = torch.cuda.is_available()
-	#print(f"is_cuda:{is_cuda}")
 
 	device = torch.device('cuda' if is_cuda else 'cpu')
-	#criterion = torch.nn.BCELoss()
 
 	def rampup(epoch):
 		 if epoch < rampup_length:
@@ -117,12 +96,12 @@ for i in range(n):
 
 
 
-	def get_num_samples(data_loader):
-		num_graph_in_last_batch = list(data_loader)[-1].num_graphs
-		total = (len(data_loader)-1)* batch_size + num_graph_in_last_batch
-		
-		#print(f"len(data_loader):{len(data_loader)}, last batch:{num_graph_in_last_batch},  total:{total}")
-		return total 
+#	def get_num_samples(data_loader):
+#		num_graph_in_last_batch = list(data_loader)[-1].num_graphs
+#		total = (len(data_loader)-1)* batch_size + num_graph_in_last_batch
+#		
+#		#print(f"len(data_loader):{len(data_loader)}, last batch:{num_graph_in_last_batch},  total:{total}")
+#		return total 
 
 	train_auc_per_epoch = ['train']
 	val_auc_per_epoch = ['val']
@@ -131,14 +110,8 @@ for i in range(n):
 
 
 
-
-
-
-
 	num_train, num_labels, num_unlabeled = get_stats(col)
-	#scaled_unsupervised_weight = ini_scaled_unsupervised_weight * float(num_labels) / float(num_train)
 	test_sc = 0
-	#model = MyNet(num_node_features, num_edge_features, conv_depth).to(device)#Get_Net(num_node_features, num_edge_features, conv_depth, inner_atom_dim, dropout_rate).to(device)
 	args = {'inner_atom_dim': inner_atom_dim, 'sup_dropout_rate': sup_dropout_rate, 'conv_depth':conv_depth, 'unsup_dropout_rate': unsup_dropout_rate}
 	model = build_model(device, **args)
 	optimizer = torch.optim.Adam(model.parameters(), lr = lr_init)#, weight_decay = 5e-4)
@@ -154,9 +127,6 @@ for i in range(n):
 		unsupervised_weight = rampup_val * w
 		train(model, train_loader,  col, unsupervised_weight, device, optimizer, use_SSL = use_SSL, debug_mode = False, edge_dropout_rate = edge_dropout_rate)#epoch==(num_epoches-1))
 		scheduler.step()
-		#train_sc = test(train_loader, False)#  epoch==(num_epoches-1))
-		#print(f"Epoch:{epoch:03d}, Train AUC:{train_sc: .4f}, Test AUC:{test_sc: .4f}")
-		#print(f"Epoch:{epoch:03d}, Test AUC:{test_sc: .4f}")
 		train_sc = round(test(model, train_loader, False, col, device),4)
 		val_sc = round(test(model, val_loader, False, col, device),4)
 		test_sc = round(test(model, test_loader, False, col, device),4)

abd084b714f6d5152ef0a055fed2da5c4256a976
LanceKnight <lanceknight26@gmail.com>
2021-05-11 20:57:24 -0500

    make similarity_matrix tensor

--- dataset_cv.py
+++ dataset_cv.py
@@ -116,6 +116,7 @@ class Sider(InMemoryDataset):
 					similarity = DataStructs.FingerprintSimilarity(fp, past_fp)
 					self.similarity_matrix[i][j] = self.similarity_matrix[j][i]= similarity
 					#print(f"smi_sc:{similarity}")
+			self.similarity_matrix = torch.tensor(self.similarity_matrix)
 			torch.save(self.collate(data_list), self.processed_paths[0])
 			torch.save(self.similarity_matrix, self.processed_paths[1])
 		#print(self.similarity_matrix)	
@@ -123,7 +124,7 @@ class Sider(InMemoryDataset):
 
 #SIDER = MoleculeNet(root = "data/SIDER", name = "SIDER")
 SIDER = Sider(root = "data/SIDER/sider", name = 'sider_pcba')
-#print(SIDER.similarity_matrix)
+#print(type(SIDER.similarity_matrix))
 #SIDER = MoleculeNet(root = "/home/liuy69/.clearml/venvs-builds/3.6/task_repository/PropertyPredictor.git/side_effect/final_model/data", name = "SIDER")# This is a combined dataset, the first 1427 samples are labeld from SIDER. Then 8597 sampes from ToxCast (19 of them were discarded due to the failure to convert to mol), 7831 samples were from Tox21. The total number of samples are 1427+8597+7831-19 = 17836
 
 NUM_LABELED = 1427

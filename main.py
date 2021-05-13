a2dde5c093e3c0af724c981690e66c9b0ffbfb3f
LanceKnight <lanceknight26@gmail.com>
2021-05-13 15:49:01 -0500

    fix a loss-chart display bug: three supvsied loss are not separated. Same for unsupervised loss

--- main.py
+++ main.py
@@ -167,13 +167,13 @@ for param_set_id in range(num_param_sets):
 			test_sc, test_loss, test_supervised_loss, test_unsupervised_loss =   map(lambda x: round(x,4),test(model= model, data_loader = test_loader,  target_col =col, device = device, method = method, unsupervised_weight = unsupervised_weight, use_SSL = use_SSL, **unsupervised_param))
 	
 
-			logger.report_scalar(title=f'supervised loss for param set {param_set_id}', series = 'supervised loss', value =train_supervised_loss,  iteration = epoch)
-			logger.report_scalar(title=f'supervised loss for param set {param_set_id}', series = 'supervised loss', value =val_supervised_loss,  iteration = epoch)
-			logger.report_scalar(title=f'supervised loss for param set {param_set_id}', series = 'supervised loss', value =test_supervised_loss,  iteration = epoch)
+			logger.report_scalar(title=f'supervised loss for param set {param_set_id}', series = 'train supervised loss', value =train_supervised_loss,  iteration = epoch)
+			logger.report_scalar(title=f'supervised loss for param set {param_set_id}', series = 'validation supervised loss', value =val_supervised_loss,  iteration = epoch)
+			logger.report_scalar(title=f'supervised loss for param set {param_set_id}', series = 'test supervised loss', value =test_supervised_loss,  iteration = epoch)
 
-			logger.report_scalar(title=f'raw unsupervised loss for param set {param_set_id}', series = 'unsupervised loss', value =train_unsupervised_loss,  iteration = epoch)
-			logger.report_scalar(title=f'raw unsupervised loss for param set {param_set_id}', series = 'unsupervised loss', value =val_unsupervised_loss,  iteration = epoch)
-			logger.report_scalar(title=f'raw unsupervised loss for param set {param_set_id}', series = 'unsupervised loss', value =test_unsupervised_loss,  iteration = epoch)
+			logger.report_scalar(title=f'raw unsupervised loss for param set {param_set_id}', series = 'train unsupervised loss', value =train_unsupervised_loss,  iteration = epoch)
+			logger.report_scalar(title=f'raw unsupervised loss for param set {param_set_id}', series = 'validation unsupervised loss', value =val_unsupervised_loss,  iteration = epoch)
+			logger.report_scalar(title=f'raw unsupervised loss for param set {param_set_id}', series = 'test unsupervised loss', value =test_unsupervised_loss,  iteration = epoch)
 		else: # === only report supervised loss if not using semi-supervised learning
 			train_sc, train_loss2= map(lambda x: round(x,4),test(model= model, data_loader = train_loader,  target_col =col, device = device, method = method, unsupervised_weight = unsupervised_weight, use_SSL = use_SSL, **unsupervised_param))
 			val_sc, val_loss =   map(lambda x: round(x,4),test(model= model, data_loader = val_loader,  target_col =col, device = device, method = method, unsupervised_weight = unsupervised_weight, use_SSL = use_SSL, **unsupervised_param))

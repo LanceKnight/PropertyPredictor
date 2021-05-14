26cd410f84925e8fc08f6ce672d6b344f796aac2
LanceKnight <lanceknight26@gmail.com>
2021-05-14 15:37:00 -0500

    add prints

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
 import numpy as np
 from statistics import mean
 from itertools import compress
-
+from termcolor import cprint
 
 from molecule_processing import batch2attributes
 from training import BCELoss_no_NaN, get_loss
@@ -36,6 +36,7 @@ def test(model=None, data_loader=None,  target_col=None, device=None, method = N
 		out, z = model(data.x.float(), data.edge_index, data.edge_attr, data.smiles, data.batch, is_supervised = True) # use our own x and edge_attr instead of data.x and data.edge_attr
 		y = data.y[:,target_col]
 
+		print(f"y:{y}, id:{data.id}, auc_lst:{auc_lst}")
 		if use_SSL:
 			loss, supervised_loss, unsupervised_loss  = get_loss(method= method, data = data, model= model, predicted = (out,z),y = y, device = device, use_SSL=use_SSL, unsupervised_weight = unsupervised_weight, **kwargs)
 			supervised_loss_lst.append(supervised_loss.item())
@@ -49,19 +50,20 @@ def test(model=None, data_loader=None,  target_col=None, device=None, method = N
 		out = out.cpu().detach().numpy()
 		#print(f"out:{out}")
 		y = y.view(len(y)).cpu().detach().numpy()
-		#print(f"y:{y}")
+		print(f"y:{y}")
 		#==========remove NaN
 		out = out[~np.isnan(y)]
 		y = y[~np.isnan(y)]
 		#print(f"loss:{loss}")
-
+		print(f"processed_y:{y}, out:{out}")
 		#print(f"data.y.shape:{y}   out.shape:{out})")
 		if ((len(y)!=0)  and (len(set(y))!=1)):
 			sc = roc_auc_score_one_class_compatible(y, out)
 			#sc = pr_auc(y, out)
 			auc_lst.append(sc)
+			cprint(f"-----sc:{sc}, auc_lst:{auc_lst}", 'red')
 
-
+	print(f"auc_lst:{auc_lst}")
 	if use_SSL:
 		try:
 			return mean(auc_lst), mean(loss_lst), mean(supervised_loss_lst), mean(unsupervised_loss_lst)
